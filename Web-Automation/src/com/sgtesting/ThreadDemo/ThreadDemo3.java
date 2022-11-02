6704d455b84edb2a9b2e154d36fd0b2ad8b67207
laxmivpawar <pawarlaxmi20@gmail.com>
2022-11-02 19:29:44 +0530

    Thread Safe

---
+++ Web-Automation/src/com/sgtesting/ThreadDemo/ThreadDemo3.java
@@ -0,0 +1,46 @@
+package com.sgtesting.ThreadDemo;
+
+class MyThread2 implements Runnable
+{
+	public void run()
+	{
+		displayEvenNumbers();
+	}
+	synchronized void displayEvenNumbers()
+	{
+		try
+		{
+			for(int i=20;i<=40;i++)
+			{
+				if(i%2==0)
+				{
+					Thread.sleep(1000);
+					System.out.println(Thread.currentThread().getName()+"Display EvenNumbers:" +i);
+					
+				}
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+public class ThreadDemo3 {
+
+	public static void main(String[] args) {
+		MyThread2 job=new MyThread2();
+		Thread t1=new Thread(job);
+		Thread t2=new Thread(job);
+		
+		t1.setName("alpha");
+		t2.setName("Omega");
+		
+		t1.start();
+		t2.start();
+
+	}
+
+}
